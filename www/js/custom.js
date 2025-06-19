cefb35801703de4ad6d21acd20e9037e4a99573f
Robin Ochieng <robinochieng73@gmail.com>
2025-06-19 15:41:19 +0300

    Added a javascript function to hel move the sidebar together with the menu container

--- www/js/custom.js
+++ www/js/custom.js
@@ -49,3 +49,70 @@ Shiny.addCustomMessageHandler("printPage", function(message) {
   window.print();
 });
 
+// Add this JavaScript to your app to handle sidebar collapse
+// Put this in your tags$script() section or in a separate JS file
+
+$(document).ready(function() {
+  // Function to adjust menu container position based on sidebar state
+  function adjustMenuContainer() {
+    var isCollapsed = $('body').hasClass('sidebar-collapse');
+    var menuContainer = $('.menu-container');
+    var logos = $('.logos');
+    
+    if (isCollapsed) {
+      // Sidebar is collapsed
+      menuContainer.css({
+        'left': '0',
+        'width': '60px'  // Typical collapsed sidebar width
+      });
+      
+      logos.css({
+        'left': '0',
+        'width': '60px',
+        'padding': '10px 5px'
+      });
+      
+      // Hide text in menu items
+      $('.menu-container .nav-link p, .menu-container .nav-link span:not(.fa)').hide();
+      
+    } else {
+      // Sidebar is expanded
+      menuContainer.css({
+        'left': '0',
+        'width': '260px'  // Your normal sidebar width
+      });
+      
+      logos.css({
+        'left': '0',
+        'width': '260px',
+        'padding': '20px 10px 10px 10px'
+      });
+      
+      // Show text in menu items
+      $('.menu-container .nav-link p, .menu-container .nav-link span:not(.fa)').show();
+    }
+  }
+  
+  // Listen for sidebar toggle button clicks
+  $('[data-widget="pushmenu"]').on('click', function() {
+    setTimeout(adjustMenuContainer, 300);  // Wait for animation
+  });
+  
+  // Also listen for body class changes
+  var observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+      if (mutation.attributeName === "class") {
+        adjustMenuContainer();
+      }
+    });
+  });
+  
+  observer.observe(document.body, {
+    attributes: true,
+    attributeFilter: ['class']
+  });
+  
+  // Initial adjustment
+  adjustMenuContainer();
+});
+
